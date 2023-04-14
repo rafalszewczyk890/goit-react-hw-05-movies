2c3771d8a964c6f076980f4c397d1814a83c3315
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-04-14 10:02:17 +0200

    Searching actually works

--- src/components/pages/MoviesPage.jsx
+++ src/components/pages/MoviesPage.jsx
@@ -1,19 +1,35 @@
 import { Searchbar } from 'components/Searchbar';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { Trending } from 'components/Trending';
+import { useState } from 'react';
+import axios from 'axios';
 
 export const MoviesPage = () => {
+  const API_KEY = '3734bfd99a42268714d80d895f68afb5';
+  axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [foundMovies, setFoundMovies] = useState([]);
+
+  const query = searchParams.get('query');
+
+  console.log(query);
   const navigate = useNavigate();
 
-  const handleSubmit = event => {
+  const handleSubmit = async event => {
     event.preventDefault();
-    console.log(event.target[0].value);
     navigate(`?query=${event.target[0].value}`);
+    const response = await axios.get(
+      `search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`
+    );
+    setFoundMovies(response.data.results);
   };
 
   return (
     <>
       <Searchbar onSubmit={handleSubmit} />
       <div>MoviesPage</div>
+      <p>Searching for: {query}</p>
+      <Trending trendingMovies={foundMovies} />
     </>
   );
 };
