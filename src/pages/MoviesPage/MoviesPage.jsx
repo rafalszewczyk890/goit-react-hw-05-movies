1e159f811b700ae617b977155b938379a5e85319
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-04-15 10:16:36 +0200

    refined search

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -10,17 +10,24 @@ export const MoviesPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [foundMovies, setFoundMovies] = useState([]);
 
-  const navigate = useNavigate();
   const query = searchParams.get('query');
 
   const handleSubmit = async event => {
     event.preventDefault();
-    navigate(`?query=${event.target[0].value}`);
-    const response = await axios.get(
-      `search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`
-    );
-    setFoundMovies(response.data.results);
+    setSearchParams({ query: event.target[0].value });
   };
+
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await axios.get(
+        `search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`
+      );
+      setFoundMovies(response.data.results);
+    };
+    if (query !== null) {
+      fetch();
+    }
+  }, [query]);
 
   return (
     <>
