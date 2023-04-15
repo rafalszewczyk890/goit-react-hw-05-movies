a676a286f36b7a8017b9ca820ff7ae7f213f06c3
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-04-15 09:49:35 +0200

    Added error handling

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -14,6 +14,7 @@ const StyledNavLink = styled(NavLink)`
 
 export const MovieDetailsPage = () => {
   const [movie, setMovie] = useState({});
+  const [error, setError] = useState(null);
   const { movieId } = useParams();
 
   const API_KEY = '3734bfd99a42268714d80d895f68afb5';
@@ -21,33 +22,45 @@ export const MovieDetailsPage = () => {
 
   useEffect(() => {
     const fetch = async () => {
-      const response = await axios.get(
-        `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`
-      );
+      const response = await axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`
+        )
+        .catch(error => {
+          setError(error);
+        });
       setMovie(response.data);
       console.log(response.data);
+      console.log(error);
     };
     fetch();
   }, []);
 
   return (
     <>
-      <div>MovieDetailsPage: {movieId}</div>
-      <p>
-        Title: {movie.title} ({new Date(movie.release_date).getFullYear()})
-      </p>
-      <img
-        src={`https://image.tmdb.org/t/p/w300` + movie.poster_path}
-        alt={movie.title}
-      ></img>
-      <p>User score: {movie.vote_average}</p>
-      <h3>Overview</h3>
-      <div>{movie.overview}</div>
-      <h3>Genres</h3>
+      {error ? (
+        <p>Movie not found</p>
+      ) : (
+        <>
+          <div>MovieDetailsPage: {movieId}</div>
+          <p>
+            Title: {movie.title} {movie.code} (
+            {new Date(movie.release_date).getFullYear()})
+          </p>
+          <img
+            src={`https://image.tmdb.org/t/p/w300` + movie.poster_path}
+            alt={movie.title}
+          ></img>
+          <p>User score: {movie.vote_average}</p>
+          <h3>Overview</h3>
+          <div>{movie.overview}</div>
+          <h3>Genres</h3>
 
-      <StyledNavLink to="cast">See cast</StyledNavLink>
-      <StyledNavLink to="reviews">See reviews</StyledNavLink>
-      <Outlet />
+          <StyledNavLink to="cast">See cast</StyledNavLink>
+          <StyledNavLink to="reviews">See reviews</StyledNavLink>
+          <Outlet />
+        </>
+      )}
     </>
   );
 };
