74e0b15e1fe34e10ee61cfc92c00f29b1fdaccd7
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-04-16 16:34:04 +0200

    Fixed html in review

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,10 +1,12 @@
 import css from './ReviewItem.module.css';
 
 export const ReviewItem = props => {
+  const htmlreview = { __html: props.content };
+
   return (
     <div className={css.Review}>
       <p className={css.Author}>{props.author}</p>
-      <p>{props.content}</p>
+      <p dangerouslySetInnerHTML={htmlreview}></p>
     </div>
   );
 };
