44f79376ed8fffb8d0f576e63ac126549568bba8
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-04-16 15:08:09 +0200

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { ReviewItem } from '../ReviewItem/ReviewItem';
@@ -17,11 +18,8 @@ const Reviews = () => {
         `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US`
       );
       setReviews(response.data.results);
-      console.log(response.data.results);
     };
     fetch();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    // test
   }, []);
 
   if (reviews.length > 0) {
